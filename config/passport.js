5d6a48f9070e18ffa4e942d5e571b2ef9bf3f871
Zestymcspicy <halbeckerman@yahoo.com>
2019-10-14 00:24:26 -0500

    cleaned for heroku deploy

--- config/passport.js
+++ config/passport.js
@@ -2,11 +2,11 @@ const JwtStrategy = require("passport-jwt").Strategy;
 const ExtractJwt = require("passport-jwt").ExtractJwt;
 const mongoose = require("mongoose");
 const User = mongoose.model("users");
-const secret = require("../config/secret")||process.env.SECRETORKEY;
-
+// const secret = require("../config/secret")||process.env.SECRETORKEY;
+const secret = process.env.SECRETORKEY;
 const opts = {};
 opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
-opts.secretOrKey = secret.secretOrKey;
+opts.secretOrKey = secret;
 
 module.exports = passport => {
   passport.use(
